a3596c7ceb51280d3a30f48205c0d3beb925dcc2
ZhumanJiang <142194405+ZhumanJiang@users.noreply.github.com>
2024-01-15 15:26:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,24 @@
 
 
 from flask import Flask, request, render_template
-
+import google.generativeai as palm
+palm.configure(api_key="AIzaSyCpkKhtMOu_cQvPte00zyVDapIr7r00AaM")
+model = {"model":"models/chat-bison-001"}
 app = Flask(__name__)
-
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        rate = float(request.form.get("rate"))
-        print(rate)
-        return(render_template("index.html",result=90.2 +(-50.6*rate)))
+        q = request.form.get("q")
+        print(q)
+        r = palm.chat(
+            **model,
+            messages = q
+        )
+        return(render_template("index.html",result=r.last))
     else:
-        return(render_template("index.html", result="waiting for exchange rate............."))
+        return(render_template("index.html", result="waiting for question............."))
 if __name__ == "__main__":
-    app.run()
+    app.run(port=1234)
 
 
 # In[ ]:
